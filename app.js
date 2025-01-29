a8f966afdcba54aa8fb97e134a8a6e1c0a267d87
Guhanandan <guhanandan6542060@gmail.com>
2025-01-29 14:48:43 +0530

    Implemented GitOps using flux

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('This is my application with version tag v1.0.1');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About page');
+});
+
+// Start the server on port 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
